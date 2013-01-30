54257f05827567d331ccd4d907339ddbe6d101bc
Travis Keep <keep94@gmail.com>
2013-01-30 10:28:27 -0800

    Initial commit.

---
+++ session_data.go
@@ -0,0 +1,111 @@
+package ramstore
+
+import (
+  "sync"
+  "time"
+)
+
+// RAMSessions stores session data. Session data for a particular session
+// expires after a set time of inactivity for that session. RAMSessions can
+// be safely used with multiple goroutines. Clients should not use this type
+// directly, but should use RAMStore instead.
+type RAMSessions struct {
+  mutex sync.Mutex
+  data map[string]*ramSession
+  clock func() int64
+  maxAge int64
+}
+
+// NewRAMSessions creates a new RAMSessions instance. maxAge is the maximum
+// allowed inactivity in seconds before data for a particular session expires.
+func NewRAMSessions(maxAge int) *RAMSessions {
+  return newRAMSessionsForTesting(maxAge, nowInSeconds)
+}
+
+func newRAMSessionsForTesting(maxAge int, clock func() int64) *RAMSessions {
+  result := &RAMSessions{
+      data: make(map[string]*ramSession),
+      clock: clock,
+      maxAge: int64(maxAge)}
+  go func() {
+    for {
+      <-time.After(time.Duration(maxAge) * time.Second)
+      result.Purge()
+    }
+  }()
+  return result
+}
+
+
+// Get returns a shallow copy of the session data for a particular session ID.
+// Get returns nil if the session ID does not exist or if the session data
+// for the session ID expired from too much inactivity.
+func (r *RAMSessions) Get(id string) map[interface{}]interface{} {
+  r.mutex.Lock()
+  defer r.mutex.Unlock()
+  ramSession := r.data[id]
+  if ramSession == nil {
+    return nil
+  }
+  return ramSession.Get(r.clock(), r.maxAge)
+}
+
+// Save saves new session data for a particular session ID.
+// Save makes a shallow copy of data before saving it.
+func (r *RAMSessions) Save(id string, data map[interface{}]interface{}) {
+  r.mutex.Lock()
+  defer r.mutex.Unlock()
+  r.data[id] = newRAMSession(data, r.clock())
+}
+
+// Purge removes session data that has already expired. Clients need not call
+// this manually as a separate go routine calls this periodically.
+func (r *RAMSessions) Purge() {
+  r.mutex.Lock()
+  defer r.mutex.Unlock()
+  now := r.clock()
+  for k, v := range r.data {
+    if v.Expired(now, r.maxAge) {
+      delete(r.data, k)
+    }
+  }
+}
+
+func (r *RAMSessions) lenForTesting() int {
+  r.mutex.Lock()
+  defer r.mutex.Unlock()
+  return len(r.data)
+}
+
+type ramSession struct {
+  data map[interface{}]interface{}
+  lastAccessed int64
+}
+
+func newRAMSession(data map[interface{}]interface{}, now int64) *ramSession {
+  return &ramSession{copyMap(data), now}
+}
+
+func (r *ramSession) Get(now int64, maxAge int64) map[interface{}]interface{} {
+  if r.Expired(now, maxAge) {
+    return nil
+  }
+  r.lastAccessed = now
+  return copyMap(r.data)
+}
+
+func (r *ramSession) Expired(now int64, maxAge int64) bool {
+  return now - r.lastAccessed > maxAge
+}
+
+func nowInSeconds() int64 {
+  return time.Now().Unix()
+}
+
+func copyMap(data map[interface{}]interface{}) map[interface{}]interface{} {
+  result := make(map[interface{}]interface{}, len(data))
+  for k, v := range data {
+    result[k] = v
+  }
+  return result
+}
---
+++ store.go
@@ -0,0 +1,80 @@
+// Package ramstore implements an in-memory session store for Gorilla Web Toolkit.
+// In-memory sessions expire after a set time of inactivity.
+package ramstore
+
+import (
+  "encoding/base32"
+  "github.com/gorilla/securecookie"
+  "github.com/gorilla/sessions"
+  "net/http"
+  "strings"
+)
+
+// RAMStore is an in-memory session store for Gorilla Web Toolkit. This store
+// makes shallow copies of maps, so value objects such as string and int can be
+// safely used with in-memory sessions with no regard for synchronization.
+// Care must be taken with reference types such as pointers, maps, or slices.
+// To what these reference refer must be treated as frozen to prevent
+// contention with other go-routines.
+type RAMStore struct {
+  Options *sessions.Options
+  Data *RAMSessions
+}
+  
+// NewRAMStore creates a new in-memory session store. maxAge is the maximum
+// time of inactivity in seconds before a session expires.
+func NewRAMStore(maxAge int) *RAMStore {
+  return &RAMStore{
+      Options: &sessions.Options {
+          Path: "/"},
+      Data: NewRAMSessions(maxAge)}
+}
+
+// Get retrieves the session. name is the name of the cookie storing the
+// session ID. If Get is called a second time with the same request pointer,
+// the session is retrieved from the request's context rather than from this
+// store. Callers should call context.Clear() in a defer statement after
+// calling Get.
+func (s *RAMStore) Get(r *http.Request, name string) (*sessions.Session, error) {
+  return sessions.GetRegistry(r).Get(s, name)
+}
+
+// New fetches the session from this store. name is the name of the cookie
+// holding the session ID. Get calls New if the session is not already cached
+// in the request's context. This implementation of New never returns a non-nil
+// error.
+func (s *RAMStore) New(r *http.Request, name string) (*sessions.Session, error) {
+  session := sessions.NewSession(s, name)
+  session.Options = &(*s.Options)
+  session.IsNew = true
+  if c, errCookie := r.Cookie(name); errCookie == nil {
+    session.ID = c.Value
+    s.load(session)
+  }
+  return session, nil
+}
+
+// Save saves a session to the store. If the session has no ID, Save assigns
+// a random one. This implementation of Save never returns a non-nil error.
+func (s *RAMStore) Save(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
+  if session.ID == "" {
+    session.ID = strings.TrimRight(
+        base32.StdEncoding.EncodeToString(
+            securecookie.GenerateRandomKey(32)), "=")
+  }
+  s.save(session)
+  http.SetCookie(w, sessions.NewCookie(session.Name(), session.ID, session.Options))
+  return nil
+}
+
+func (s *RAMStore) save(session *sessions.Session) {
+  s.Data.Save(session.ID, session.Values)
+}
+
+func (s *RAMStore) load(session *sessions.Session) {
+  sessionData := s.Data.Get(session.ID)
+  if sessionData != nil {
+    session.Values = sessionData
+    session.IsNew = false
+  }
+}
